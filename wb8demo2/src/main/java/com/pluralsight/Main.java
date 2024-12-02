5653fc6ebe4c62b54e49a18727426e04f9f530ab
agabin <agabin@appdev.yearup.org>
2024-12-02 14:58:44 -0500

    updates

--- wb8demo2/src/main/java/com/pluralsight/Main.java
+++ wb8demo2/src/main/java/com/pluralsight/Main.java
@@ -5,6 +5,36 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args){
 
+        if (args.length != 3) {
+            System.out.println(
+                    "Application needs two arguments to run: " +
+                            "java com.pluralsight.wb8demo2 <username> <password> <server>");
+            System.exit(1);
+        }
+
+        // get the username and password from the command line args
+        String username = args[0];
+        String password = args[1];
+        String sqlServerAddress = args[2];
+
+
+        try {
+            doDatabaseStuff(username, password, sqlServerAddress);
+        } catch (Exception e) {
+            System.out.println("Catch in main...");
+            e.printStackTrace();
+        }
+
+        System.out.println("Very end of application...");
+
+
+    }
+
+    public static void doDatabaseStuff(String username, String password, String sqlServerAddress) throws SQLException, ClassNotFoundException {
+
+        Connection connection = null;
+        PreparedStatement pStatement = null;
+        ResultSet results = null;
         try{
 
             // load the MySQL Driver
@@ -12,34 +42,55 @@ public class Main {
 
             // 1. open a connection to the database
             // use the database URL to point to the correct database
-            Connection connection;
-            connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/sakila",
-                    "root",
-                    "yearup");
-            // create statement
-            // the statement is tied to the open connection
-            Statement statement = connection.createStatement();
-            // define your query
-            String query = "SELECT * FROM sakila.city WHERE country_id = 103;";
-            // 2. Execute your query
-            ResultSet results = statement.executeQuery(query);
+
+            connection = DriverManager.getConnection(sqlServerAddress, username, password);
+
+            int country_id = 103;
+
+            pStatement = connection.prepareStatement("SELECT * FROM sakila.city WHERE country_id = ?;");
+            pStatement.setInt(1, country_id);
+
+            results = pStatement.executeQuery();
             // process the results
             while (results.next()) {
+                int cityId = results.getInt("city_id");
                 String city = results.getString("city");
+                System.out.println(cityId);
                 System.out.println(city);
-            }
-            // 3. Close the connection
-            connection.close();
 
-        } catch (ClassNotFoundException e) {
-            System.out.println("There was an issue finding a class:");
-            e.printStackTrace();
+            }
+
         }
-        catch (SQLException e) {
-            System.out.println("There was an SQL issue:");
-            e.printStackTrace();
+        catch(Exception e){
+            //collect additional information about the problem...
+            //log e, and additional information....
+            System.out.println("Catch inside doDatabaseStuff...");
+            log(sqlServerAddress);
+            log(username);
+            log(e);
+            throw(e);
         }
+
+        finally {
+            System.out.println("Cleaning up database objects in finally...");
+            if(results != null) { results.close();}
+            if(pStatement != null) {pStatement.close();}
+            if(connection != null) { connection.close();}
+        }
+
+
+
+
+
 
     }
+
+    public static void log(String data){
+        System.out.println("Logging: " + data);
+    }
+
+    public static void log(Object data){
+        System.out.println("Logging" + data.toString());
+    }
+
 }
