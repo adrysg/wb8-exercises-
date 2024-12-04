9a713fbfd8ed352361432b1ea15d2f236b1886a4
agabin <agabin@appdev.yearup.org>
2024-12-04 15:54:14 -0500

    Completed exercise 4

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -1,10 +1,18 @@
 package com.pluralsight;
-
 import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // load the MySQL Driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Cannot load SQL driver...");
+            System.exit(1);
+        }
 
         if (args.length != 2) {
             System.out.println(
@@ -18,38 +26,39 @@ public class Main {
         String password = args[1];
 
 
-        Scanner scanner = new Scanner(System.in);
+
         boolean running = true;
 
-        while (running){
-            System.out.println("Please select from the following options: ");
-            System.out.println("1) Display All Products");
-            System.out.println("2) Display All Customers");
-            System.out.println("0) Exit");
-            System.out.print("Enter selection: ");
-            int option = scanner.nextInt();
+        try {
+            while (running) {
+                System.out.println("Please select from the following options: ");
+                System.out.println("1) Display All Products");
+                System.out.println("2) Display All Customers");
+                System.out.println("3) Display All Categories");
+                System.out.println("0) Exit");
+                System.out.print("Enter selection: ");
+                int option = scanner.nextInt();
 
-            switch (option){
-                case 1 -> displayProducts(username, password);
-                case 2 -> displayCustomer(username, password);
-                case 0 -> running = false;
-                default -> System.out.println("Invalid selection, please try again.");
+                switch (option) {
+                    case 1 -> displayProducts(username, password);
+                    case 2 -> displayCustomer(username, password);
+                    case 3 -> displayCategories(username, password);
+                    case 0 -> running = false;
+                    default -> System.out.println("Invalid selection, please try again.");
+                }
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public static void displayProducts(String username, String password){
+    private static void displayProducts(String username, String password) {
 
-        try{
-
-            // load the MySQL Driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
+        try {
             // 1. open a connection to the database
             // use the database URL to point to the correct database
-            Connection connection;
 
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
 
             // create statement
             // the statement is tied to the open connection
@@ -74,34 +83,26 @@ public class Main {
             pStatement.close();
             connection.close();
 
-        } catch (ClassNotFoundException e) {
-            System.out.println("There was an issue finding a class:");
-            e.printStackTrace();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("There was an SQL issue:");
             e.printStackTrace();
         }
     }
 
-    private static void displayCustomer(String username, String password){
-        try{
+    private static void displayCustomer(String username, String password) {
 
-            // load the MySQL Driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
+        try {
             // 1. open a connection to the database
             // use the database URL to point to the correct database
-            Connection connection;
-
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);
 
             // create statement
             // the statement is tied to the open connection
-            PreparedStatement pStatement = connection.prepareStatement("SELECT ContactName, CompanyName, City, Country, Phone\n" +
-                    "FROM northwind.customers\n" +
-                    "WHERE Country IS NOT NULL\n" +
-                    "ORDER BY Country;");
+            PreparedStatement pStatement = connection.prepareStatement("""
+                    SELECT ContactName, CompanyName, City, Country, Phone
+                    FROM northwind.customers
+                    WHERE Country IS NOT NULL
+                    ORDER BY Country;""");
 
             // 2. Execute your query
             ResultSet results = pStatement.executeQuery();
@@ -124,15 +125,70 @@ public class Main {
             pStatement.close();
             connection.close();
 
-        } catch (ClassNotFoundException e) {
-            System.out.println("There was an issue finding a class:");
-            e.printStackTrace();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("There was an SQL issue:");
             e.printStackTrace();
         }
 
     }
-    
+
+    private static void displayCategories(String username, String password) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. open a connection to the database
+        // use the database URL to point to the correct database
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password);) {
+
+            // create statement
+            // the statement is tied to the open connection
+            PreparedStatement pStatement = connection.prepareStatement("""
+                    SELECT CategoryID, CategoryName
+                    FROM northwind.categories
+                    ORDER BY CategoryID;""");
+            {
+
+            // 2. Execute your query
+            // process the results
+                try (ResultSet results = pStatement.executeQuery()) {
+                    while (results.next()) {
+                        int categoryID = results.getInt("CategoryID");
+                        String categoryName = results.getString("CategoryName");
+                        System.out.println("Category ID: " + categoryID);
+                        System.out.println("Category Name: " + categoryName);
+                        System.out.println("----------------------------------------");
+                    }
+                }
+
+                System.out.print("\nPlease enter a Category ID: ");
+                int selectedCategoryID = scanner.nextInt();
+                System.out.println("----------------------------------------");
+
+                try (PreparedStatement statement = connection.prepareStatement("""
+                    SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+                    FROM northwind.products
+                    WHERE CategoryID = ?;""")) {
+
+                    statement.setInt(1, selectedCategoryID);
+
+                    try (ResultSet idresults = statement.executeQuery()) {
+                        while (idresults.next()) {
+                            int productID = idresults.getInt("ProductID");
+                            String productName = idresults.getString("ProductName");
+                            double unitPrice= idresults.getDouble("UnitPrice");
+                            int unitsInStock = idresults.getInt("UnitsInStock");
+
+                            System.out.println("Product ID: " + productID);
+                            System.out.println("Product Name: " + productName);
+                            System.out.println("Unit Price: $" + unitPrice);
+                            System.out.println("Units in stock: " + unitsInStock);
+                            System.out.println("----------------------------------------");
+                        }
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("There was an SQL issue: ");
+            e.printStackTrace();
+        }
+    }
 }
